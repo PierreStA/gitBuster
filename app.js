9ac677692f973da5ed5dda72b6680c2e6538a97c
Debord <debordantoine0@gmail.com>
2022-12-06 20:38:07 +0100

    leger changement lignes

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-let img__slider = document.getElementsByClassName('img__slider');
-
+let imgSlider = document.getElementsByClassName("imgSlider");
+const dev = document.querySelector(".devName");
 let etape = 0;
+const devName = ["Antoine", "Cassandra", "Claudia", "David", "Pierre"];
+const newH2 = document.createElement("h2"); //creation du h2
+newH2.textContent = devName[0]; //creation du contenu
+dev.append(newH2); //insertion du h2
 
-let nbr__img = img__slider.length;
+let nbrImg = imgSlider.length;
 
-let precedent = document.querySelector('.precedent');
-let suivant = document.querySelector('.suivant');
+let precedent = document.querySelector(".precedent");
+let suivant = document.querySelector(".suivant");
 
 function enleverActiveImages() {
-    for(let i = 0 ; i < nbr__img ; i++) {
-        img__slider[i].classList.remove('active');
-    }
+  for (let i = 0; i < nbrImg; i++) {
+    imgSlider[i].classList.remove("active");
+  }
 }
 
-suivant.addEventListener('click', function() {
-    etape++;
-    if(etape >= nbr__img) {
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
+suivant.addEventListener("click", function () {
+  etape++;
+  if (etape >= nbrImg) {
+    etape = 0;
+  }
+  enleverActiveImages();
+  changeName();
+  imgSlider[etape].classList.add("active");
+});
 
-precedent.addEventListener('click', function() {
-    etape--;
-    if(etape < 0) {
-        etape = nbr__img - 1;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-})
+precedent.addEventListener("click", function () {
+  etape--;
+  if (etape < 0) {
+    etape = nbrImg - 1;
+  }
+  enleverActiveImages();
+  changeName();
+  imgSlider[etape].classList.add("active");
+});
 
-setInterval(function() {
-    etape++;
-    if(etape >= nbr__img) {
-        etape = 0;
-    }
-    enleverActiveImages();
-    img__slider[etape].classList.add('active');
-}, 3000)
+// setInterval(function() {
+//    etape++;
+//    if(etape >= nbrImg) {
+//        etape = 0;
+//        changeName()
+//    }
+//    enleverActiveImages();
+//    changeName()
+//    imgSlider[etape].classList.add('active');
+// }, 2000)
+
+function changeName() {
+  const newH2 = document.createElement("h2"); //creation du h2
+  newH2.textContent = devName[etape]; //creation du contenu
+  dev.append(newH2); //insertion du h2
+  dev.firstChild.remove();
+}
